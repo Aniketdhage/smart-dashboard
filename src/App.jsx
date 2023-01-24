121df72feb77e0d65b26313e4c44429aa2c1e341
Aniketdhage <aniketdhage90@gmail.com>
2023-01-24 10:53:55 +0530

    update : installed tailwind css and created folders in src folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
-  return <h1 className="underline">Hello World</h1>;
+  return <h1 className="underline bg-orange-600">Hello World</h1>;
 };
 
 export default App;
