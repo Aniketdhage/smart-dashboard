3f9d13af6e6e1efd63f4585defdb4c05b94b3e13
Aniketdhage <aniketdhage90@gmail.com>
2023-01-28 09:06:40 +0530

    update : created navbar component and added functionality to sidebar close,open

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,14 @@ import { links } from "./../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setAcitveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleSidebarClose = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
@@ -19,7 +26,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to={"/"}
-              onClick={() => setAcitveMenu(false)}
+              onClick={handleSidebarClose}
               className="items-center gap-3 mt-4 ml-3 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Smart Dashboard</span>
@@ -28,7 +35,7 @@ const Sidebar = () => {
               <button
                 type="button"
                 className="text-xl rounded-full mt-4 block  "
-                onClick={() => setAcitveMenu((prev) => !prev)}
+                onClick={() => setActiveMenu((prev) => !prev)}
               >
                 <MdOutlineCancel />
               </button>
@@ -47,7 +54,7 @@ const Sidebar = () => {
                       <NavLink
                         to={`/${link.name}`}
                         key={link.name}
-                        onClick={() => {}}
+                        onClick={handleSidebarClose}
                         className={({ isActive }) =>
                           isActive ? activeLink : normalLink
                         }
