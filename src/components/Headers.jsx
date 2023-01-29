b913b4388b836aef1980e38d2393192e165c8151
Aniketdhage <aniketdhage90@gmail.com>
2023-01-29 21:01:59 +0530

    update : added orders page with fully responsive and functional table

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Headers = () => {
-  return <div>Headers</div>;
+const Headers = ({ category, title }) => {
+  return (
+    <div className="mb-10">
+      <p className="text-gray-400">{category}</p>
+      <p className="text-3xl tracking-tight text-slate-900 font-extrabold">
+        {title}
+      </p>
+    </div>
+  );
 };
 
 export default Headers;
