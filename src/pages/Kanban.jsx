2af6faa63f4f7b4b431b0799f544c68af583070b
Aniketdhage <aniketdhage90@gmail.com>
2023-01-30 17:42:59 +0530

    update : created kanban component with drag and drop functionality

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import {
-  Kanban,
+  KanbanComponent,
   ColumnsDirective,
   ColumnDirective,
 } from "@syncfusion/ej2-react-kanban";
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Headers } from "../components";
 
 const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl ">
+      <Headers title="Kanban" category="App" />
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+      >
+        <ColumnsDirective>
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
 };
 
 export default Kanban;
