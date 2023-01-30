0d18fa0ccbcb75756fbe02013a06a682f3b34190
Aniketdhage <aniketdhage90@gmail.com>
2023-01-30 15:39:40 +0530

    update : added calendar component with full calendar functionality

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  Resize,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { scheduleData } from "../data/dummy";
+import { Headers } from "../components";
 
 const Calender = () => {
-  return <div>Calender</div>;
+  console.log("demo present in jan 2021");
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Headers title="Calender" category={"App"} />
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{
+          dataSource: scheduleData,
+        }}
+        // selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject
+          services={[Resize, Day, Week, WorkWeek, Month, Agenda, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default Calender;
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  Kanban,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
 
 const Kanban = () => {
   return <div>Kanban</div>;
