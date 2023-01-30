696d805fd4fe139d095e741d475753bad8bed6c0
Aniketdhage <aniketdhage90@gmail.com>
2023-01-30 17:58:47 +0530

    update : created editor component with full functionality

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import {
+  RichTextEditorComponent,
+  Inject,
+  Image,
+  Link,
+  HtmlEditor,
+  Toolbar,
+  QuickToolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
+import { Headers } from "../components";
+import { EditorData } from "../data/dummy";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Headers category="App" title="Editor" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
