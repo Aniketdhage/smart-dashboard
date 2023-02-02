45ececd3201d856114ef0a965ffb0f8a16efc078
Aniketdhage <aniketdhage90@gmail.com>
2023-02-02 12:26:48 +0530

    update : build folder

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,43 @@
 import React from "react";
-
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  SplineAreaSeries,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  areaCustomSeries,
+  areaPrimaryYAxis,
+  areaPrimaryXAxis,
+} from "../../data/dummy";
+import { Headers } from "../../components";
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Headers category="Chart" title="Inflamation Rate In Percentage" />
+      <ChartComponent
+        id="area_chart"
+        primaryXAxis={areaPrimaryXAxis}
+        primaryYAxis={areaPrimaryYAxis}
+        tooltip={{ enable: true }}
+        chartArea={{ border: { width: 0 } }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      >
+        <Inject services={[DateTime, SplineAreaSeries, Legend, Tooltip]} />
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Area;
--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,7 +1,48 @@
 import React from "react";
-
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  Tooltip,
+  DataLabel,
+  ColumnSeries,
+} from "@syncfusion/ej2-react-charts";
+import { useStateContext } from "../../contexts/ContextProvider";
+import {
+  barChartData,
+  barCustomSeries,
+  barPrimaryYAxis,
+  barPrimaryXAxis,
+} from "../../data/dummy";
+import { Headers } from "../../components";
 const Bar = () => {
-  return <div>Bar</div>;
+  const { currentMode } = useStateContext();
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Headers category="Chart" title="Inflamation Rate In Percentage" />
+      <ChartComponent
+        id="charts"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        legendSettings={{ background: "white" }}
+      >
+        <Inject
+          services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}
+        />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Bar;
