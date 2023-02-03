6ea951305b46a6e4f403353486c38ca8d5f6c346
Aniketdhage <aniketdhage90@gmail.com>
2023-02-03 07:41:52 +0530

    change : line chart temporary closed

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+// import React from "react";
 
-import { Headers, LineChart } from "../../components";
+// import { Headers, LineChart } from "../../components";
 
-const Line = () => {
-  return (
-    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Headers category="Chart" title="Inflation Rate" />
-      <div className="w-full">
-        <LineChart />
-      </div>
-    </div>
-  );
-};
+// const Line = () => {
+//   return (
+//     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+//       <Headers category="Chart" title="Inflation Rate" />
+//       <div className="w-full">
+//         <LineChart />
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Line;
+// export default Line;
